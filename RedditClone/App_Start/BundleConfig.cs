d6a72a482300f1b3e891af7dcae47437a1370683
brandon <brandonbrewer93@gmail.com>
2018-03-29 19:46:21 -0400

    Remove unused code.

--- RedditClone/App_Start/BundleConfig.cs
+++ RedditClone/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace RedditClone
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
